26c34e2560cbcebf23ed7253f0ac0f306f6bdc40
Roberto.Gonzalez1 <wowroberto80@gmail.com>
2022-07-08 13:50:22 -0400

    initial commit

---
+++ shipping.py
@@ -0,0 +1,17 @@
+# def minimalHeaviestSetA(arr):
+#     A = []
+#     B = []
+#     for i in arr:
+#         if 1 < i <= 1000000:
+#             list.append(i)
+#     return list  
+
+# arr = [4,2,5,1,6]
+# print(minimalHeaviestSetA(arr))
+
+
+n = 6
+arr = [5,3,2,4,1,2]
+total = 0
+a = []
+b = []
